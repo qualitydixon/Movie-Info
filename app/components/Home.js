2344d9f4f41c2f5a638ba7e0a895635badd1baa9
Michael Dixon <qualitydixon@gmail.com>
2016-04-18 15:23:06 -0600

    Update readme

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,26 @@
 var React = require('react');
-
+require('../stylesheets/main.css');
 var Home = React.createClass({
   render: function () {
     return (
-      <div> This is your home component, which is currently set as the IndexRoute in app/config/routes.js. It shows when no other routes are active. </div>
+      <div className='home'>
+      <div>
+        <h1> Enter a Movie Title </h1>
+        <form onSubmit={this.props.onSubmitCity}>
+            <div className='form-group' id='inputBox'>
+                <input 
+                    className='form-control' 
+                    placeholder='Star Wars' 
+                    type='text'
+                    onChange={this.props.onUpdateCity} 
+                    value={this.props.city} />
+             </div>
+            <div className='form-group' id='getWeatherBtn'>
+                <button onClick={this.props.onSubmitCity} className="btn getBtn"> Get Movies </button>
+            </div>
+        </form>
+        </div>
+      </div>
     )
   }
 });
